da8fe6815d29860c5215cac29a7b92fa90c71f8d
erasyl-ui <yelubeky@mail.ru>
2025-02-07 21:16:56 +0500

    Update ex9.py

--- functions1.md/ex9.py
+++ functions1.md/ex9.py
@@ -1,12 +1,13 @@
-"""""""
-Write a function that computes the volume of a sphere given its radius.
-"""""""
+"""Write a function that computes the volume of a sphere given its radius."""
 
 import math
 
 def sphere_volume(radius):
     return (4/3) * math.pi * (radius ** 3)
 
-# Example:
 radius = float(input("Enter the radius: "))
 print("Volume of the sphere:", sphere_volume(radius))
+
+# Example:
+# Input: 5
+# Output: 523.5987755982989
