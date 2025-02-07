d8ca24ba537bba141a5266321b9e2695390fb8ba
erasyl-ui <yelubeky@mail.ru>
2025-02-07 21:15:49 +0500

    Update ex6.py

--- functions1.md/ex6.py
+++ functions1.md/ex6.py
@@ -1,12 +1,12 @@
-"""""""
-Write a function that accepts a string from the user and 
-   - returns a sentence with the words reversed.
-   - Example: "We are ready" -> "ready are We"
-"""""""
+"""Write a function that accepts a string from the user, return a sentence with the words reversed.
+Example: We are ready -> ready are We"""
 
 def reverse_sentence(sentence):
     return " ".join(sentence.split()[::-1])
 
-# Example:
 sentence = input("Enter a sentence: ")
 print(reverse_sentence(sentence))
+
+# Example:
+# Input: We are ready
+# Output: ready are We
