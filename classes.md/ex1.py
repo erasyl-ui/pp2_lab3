62ce8bbddbb763003a2577c30ba7dbeed4b6ad45
erasyl-ui <yelubeky@mail.ru>
2025-02-07 20:40:36 +0500

    Update ex1.py

--- classes.md/ex1.py
+++ classes.md/ex1.py
@@ -1,5 +1,5 @@
 """""""
-1. Define a class which has at least two methods: 
+Define a class which has at least two methods: 
    - getString: to get a string from console input
    - printString: to print the string in upper case.
 """""""
