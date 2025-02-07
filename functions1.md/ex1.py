f78f56a224254ed0b4bc89aa90a47e2c8f2b1139
erasyl-ui <yelubeky@mail.ru>
2025-02-07 21:01:42 +0500

    Update ex1.py

--- functions1.md/ex1.py
+++ functions1.md/ex1.py
@@ -1,8 +1,13 @@
-"""A recipe you are reading states how many grams you need for the ingredient. Unfortunately,
-your store only sells items in ounces. Create a function to convert grams to ounces.
-ounces = 28.3495231 * grams"""
-def func(grams):
+"""""""
+A recipe you are reading states how many grams you need for the ingredient. 
+   - Unfortunately, your store only sells items in ounces. 
+   - Create a function to convert grams to ounces.
+   - ounces = 28.3495231 * grams
+"""""""
+
+def grams_to_ounces(grams):
     return grams * 28.3495231
-gram = float(input("how many grams: "))
-print(func(gram),"ounces")
-#400 >>> 11339.809239999999 ounces
+
+# Example:
+grams = float(input("Enter weight in grams: "))
+print(grams_to_ounces(grams), "ounces")
