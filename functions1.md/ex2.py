dfa0b22d8cef0601072c98af2b840ff17069126a
erasyl-ui <yelubeky@mail.ru>
2025-02-07 21:11:53 +0500

    Update ex2.py

--- functions1.md/ex2.py
+++ functions1.md/ex2.py
@@ -1,12 +1,13 @@
-"""""""
-Read in a Fahrenheit temperature. 
-   - Calculate and display the equivalent centigrade temperature. 
-   - Formula: C = (5 / 9) * (F – 32)
-"""""""
+"""Read in a Fahrenheit temperature. Calculate and display the equivalent centigrade temperature.
+The following formula is used for the conversion: C = (5 / 9) * (F – 32)"""
 
 def fahrenheit_to_celsius(fahrenheit):
     return (5 / 9) * (fahrenheit - 32)
 
-# Example:
 fahrenheit = float(input("Enter temperature in Fahrenheit: "))
 print(fahrenheit_to_celsius(fahrenheit), "Celsius")
+
+# Example:
+# Input: 100
+# Output: 37.77777777777778 Celsius
+
