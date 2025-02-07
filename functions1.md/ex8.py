f1e92189680d959ba5c89ab50c180526f9e35f0a
erasyl-ui <yelubeky@mail.ru>
2025-02-07 21:05:18 +0500

    Create ex8.py

---
+++ functions1.md/ex8.py
@@ -0,0 +1,16 @@
+"""""""
+Write a function that takes in a list of integers and returns True if it contains 007 in order.
+"""""""
+
+def spy_game(nums):
+    code = [0, 0, 7]
+    for num in nums:
+        if num == code[0]:
+            code.pop(0)
+        if not code:
+            return True
+    return False
+
+# Example:
+numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+print(spy_game(numbers))
