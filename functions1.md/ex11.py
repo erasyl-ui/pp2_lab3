aeee999e837e8b22f2237dcacaffda0c5784c623
erasyl-ui <yelubeky@mail.ru>
2025-02-07 21:17:48 +0500

    Update ex11.py

--- functions1.md/ex11.py
+++ functions1.md/ex11.py
@@ -1,12 +1,13 @@
-"""""""
-Write a function that checks whether a word or phrase is palindrome or not. 
-    - Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam.
-"""""""
+"""Write a Python function that checks whether a word or phrase is palindrome or not.
+Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam"""
 
 def is_palindrome(s):
     s = s.replace(" ", "").lower()
     return s == s[::-1]
 
-# Example:
 word = input("Enter a word or phrase: ")
 print("Palindrome:", is_palindrome(word))
+
+# Example:
+# Input: madam
+# Output: True
