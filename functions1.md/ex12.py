21bd107143dd61bc501506f0ce41e1245efa1c67
erasyl-ui <yelubeky@mail.ru>
2025-02-07 21:18:03 +0500

    Update ex12.py

--- functions1.md/ex12.py
+++ functions1.md/ex12.py
@@ -1,11 +1,15 @@
-"""""""
-Define a function histogram() that takes a list of integers and prints a histogram to the screen.
-"""""""
+"""Define a function histogram() that takes a list of integers and prints a histogram to the screen."""
 
 def histogram(lst):
     for num in lst:
         print("*" * num)
 
-# Example:
 numbers = list(map(int, input("Enter numbers separated by space: ").split()))
 histogram(numbers)
+
+# Example:
+# Input: 4 9 7
+# Output:
+# ****
+# *********
+# *******
