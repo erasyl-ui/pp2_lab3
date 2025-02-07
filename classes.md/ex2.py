2736f99fee88488b00cf13dd523267c418251a4f
erasyl-ui <yelubeky@mail.ru>
2025-02-07 20:38:33 +0500

    Update ex2.py

--- classes.md/ex2.py
+++ classes.md/ex2.py
@@ -1,3 +1,9 @@
+"""""""
+2. Define a class named Shape and its subclass Square. 
+   - The Square class has an init function which takes a length as argument. 
+   - Both classes have an area function which can print the area of the shape where Shape's area is 0 by default.
+"""""""
+
 class Shape:
     def area(self):
         return 0
@@ -8,6 +14,10 @@ class Square(Shape):
 
     def area(self):
         return self.length * self.length
+
+# Example:
+# square = Square(5)
+# Output: Square Area: 25
 
 square = Square(5)
 print("Square Area:", square.area())
