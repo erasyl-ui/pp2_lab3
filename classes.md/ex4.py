d84154525460519a58392d62faaee3f8092ed255
erasyl-ui <yelubeky@mail.ru>
2025-02-07 20:39:29 +0500

    Update ex4.py

--- classes.md/ex4.py
+++ classes.md/ex4.py
@@ -1,3 +1,10 @@
+"""""""
+ Write the definition of a Point class. Objects from this class should have: 
+   - A method show to display the coordinates of the point.
+   - A method move to change these coordinates.
+   - A method dist that computes the distance between 2 points.
+"""""""
+
 import math
 
 class Point:
@@ -14,6 +21,10 @@ class Point:
 
     def dist(self, other_point):
         return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+
+# Example:
+# p1 = Point(3, 4), p2 = Point(6, 8)
+# Output: Distance: 5.0
 
 p1 = Point(3, 4)
 p2 = Point(6, 8)
